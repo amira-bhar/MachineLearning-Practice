a171d5b2bf0346a080973e69cf724312674465ee
amira-bhar <bharamira2@gmail.com>
2026-03-28 05:32:58 +0100

    first commit

---
+++ Pandas-course/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+"""a=np.array([1,2,3])
+print(a)
+
+b=np.array([[(1,2,3),(5,6,4)],[(4,5,6),(7,8,9)],[[7,8,9],(1,2,3)]])
+print(b)
+
+print(b.ndim)"""
+print(np.random.randint(1,8,size=(3,3)))
